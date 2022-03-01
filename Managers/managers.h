e81999a9c93f2a31560193a4b83c69736ada67db
carloleo <carlo_leo97@outlook.it>
2022-03-01 12:56:50 +0100

    added crypto functions

--- Managers/managers.h
+++ Managers/managers.h
@@ -11,6 +11,22 @@ namespace Managers {
         int read_n(int socket, size_t amount, void* buff);
 
     }
+    namespace CryptoManager {
+        int gcm_encrypt(unsigned char *plaintext, int plaintext_len,
+                        unsigned char *aad, int aad_len,
+                        unsigned char *key,
+                        unsigned char *iv, int iv_len,
+                        unsigned char *ciphertext,
+                        unsigned char *tag);
+        int gcm_decrypt(unsigned char *ciphertext, int ciphertext_len,
+                        unsigned char *aad, int aad_len,
+                        unsigned char *tag,
+                        unsigned char *key,
+                        unsigned char *iv, int iv_len,
+                        unsigned char *plaintext);
+        int sign(unsigned char*plaintext, unsigned char* sign_key);
+        int verify_signature(unsigned  char*signature, unsigned char* pub_key);
+    }
 }
 
 
